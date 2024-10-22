f8f39b7a288e0afebcf3084e5761943f808307ff
Tomas Hestera <tomas.hestera06@gmail.com>
2024-10-22 12:58:05 +0200

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Navbar from "../components/NavBar";
+// src/app/layout.tsx
 
+import { Metadata } from "next";
+import "./globals.css";
+import Navbar from "@/components/NavBar";
+import AuthProvider from "../components/AuthProvider";
 
 export const metadata: Metadata = {
-  title: "SnapZoška",
-  description: "Created by students of SPŠE Zochova 9, Bratislava",
+  title: "ZoskaApp",
+  description: "Created by Tomas Hestera",
 };
 
 export default function RootLayout({
@@ -16,13 +18,17 @@ export default function RootLayout({
   return (
     <html lang="sk">
       <body>
+        <AuthProvider>
           <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
             <main style={{ flexGrow: 1 }}>
               {children}
             </main>
           </div>
-          <Navbar />
+          <Navbar /> 
+        </AuthProvider>
       </body>
     </html>
   );
 }
+
+
